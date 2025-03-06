dcbdced89af48bf5f919c5ddbf45eafae1296d57
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-06 18:56:58 +0530

    route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to Express JS Page...</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>This is About Page ... </h1>");
+});
+
 app.listen(PORT, () => {
   console.log(`Server Running at http://localhost:${PORT}/`);
 });
