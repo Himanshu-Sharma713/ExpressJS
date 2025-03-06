bb94de4e3c1f2cac8f9377350544e843811e915c
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-06 17:03:40 +0530

    setup express

---
+++ index.js
@@ -0,0 +1,14 @@
+// Express JS Code
+
+import express from "express";
+
+const app = express();
+const PORT = 3220;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Express JS Page...</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running at http://localhost:${PORT}/`);
+});
